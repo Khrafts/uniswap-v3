3dbcee897c8afaa7633e0687ba7c9a8501f913c0
Uyoyou Saint Uwuseba <uwusebauyoyou@gmail.com>
2025-03-28 00:38:12 +0100

    feat: partial implementation of mint function

--- src/UniswapV3Pool.sol
+++ src/UniswapV3Pool.sol
@@ -9,6 +9,9 @@ contract UniswapV3Pool {
     using Position for mapping(bytes32 => Position.Info);
     using Position for Position.Info;
 
+    error InvalidTickRange();
+    error ZeroLiquidity();
+
     int24 internal constant MIN_TICK = -887272;
     int24 internal constant MAX_TICK = -MIN_TICK;
 
@@ -32,4 +35,51 @@ contract UniswapV3Pool {
     mapping(int24 => Tick.Info) public ticks;
     // Positions info
     mapping(bytes32 => Position.Info) public positions;
+
+    constructor(
+        address token0_,
+        address token1_,
+        uint160 sqrtPriceX96_,
+        int24 tick
+    ) {
+        token0 = token0_;
+        token1 = token1_;
+
+        slot0 = Slot0({sqrtPriceX96: sqrtPriceX96_, tick: tick});
+    }
+
+    function mint(
+        address owner,
+        int24 lowerTick,
+        int24 upperTick,
+        uint128 amount
+    ) external returns (uint256 amount0, uint256 amount1) {
+        if (
+            lowerTick >= upperTick ||
+            lowerTick < MIN_TICK ||
+            upperTick > MAX_TICK
+        ) revert InvalidTickRange();
+
+        if (amount == 0) revert ZeroLiquidity();
+
+        ticks.update(lowerTick, amount);
+        ticks.update(upperTick, amount);
+
+        Position.Info storage position = positions.get(
+            owner,
+            lowerTick,
+            upperTick
+        );
+
+        position.update(amount);
+
+        amount0 = 0.998976618347425280 ether;
+        amount1 = 5000 ether;
+
+        liquidity += uint128(amount);
+
+        uint256 balance0Before;
+        uint256 balance1Before;
+
+    }
 }
