88b1d91774a76c5da0e1c1bd1a760225c11944ab
Uyoyou Saint Uwuseba <uwusebauyoyou@gmail.com>
2025-04-09 18:38:30 +0100

    feat: more on path library

--- src/lib/Path.sol
+++ src/lib/Path.sol
@@ -1,7 +1,29 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.14;
+import {BytesLib} from "solidity-bytes-utils/BytesLib.sol";
+
+library BytesLibExt {
+    function toUint24(bytes memory _bytes, uint256 _start)
+        internal
+        pure
+        returns (uint24)
+    {
+        require(_bytes.length >= _start + 3, "toUint24_outOfBounds");
+        uint24 tempUint;
+
+        assembly {
+            tempUint := mload(add(add(_bytes, 0x3), _start))
+        }
+
+        return tempUint;
+    }
+}
 
 library Path {
+
+    using BytesLib for bytes;
+    using BytesLibExt for bytes;
+
     uint256 private constant ADDR_SIZE = 20;
     uint256 private constant TICKSPACING_SIZE = 3;
     uint256 private constant NEXT_OFFSET = ADDR_SIZE + TICKSPACING_SIZE;
@@ -16,5 +38,22 @@ library Path {
         return path.length >= MULTIPLE_POOLS_MIN_LENGTH;
     }
 
-    
+    function getFirstPool(bytes memory path) internal pure returns (bytes memory) {
+        return path.slice(0, POP_OFFSET);
+    }
+
+    function skipToken(bytes memory path) internal pure returns (bytes memory) {
+        return path.slice(NEXT_OFFSET, path.length - NEXT_OFFSET);
+    }
+
+    function decodeFirstPool(bytes memory path) internal pure returns (
+        address tokenIn,
+        address tokenOut,
+        uint24 tickSpacing
+    ) {
+        tokenIn = path.toAddress(0);
+        tickSpacing = path.toUint24(ADDR_SIZE);
+        tokenOut = path.toAddress(NEXT_OFFSET);
+    }
+
 }
