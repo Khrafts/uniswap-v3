618dd8ff4fd354709ece11a1c961523fc264e86b
Uyoyou Saint Uwuseba <uwusebauyoyou@gmail.com>
2025-04-10 15:25:53 +0100

    chore: forge fmt

--- test/lib/Math.t.sol
+++ test/lib/Math.t.sol
@@ -8,9 +8,7 @@ import "../../src/lib/TickMath.sol";
 contract MathTest is Test {
     function testCalcAmount0Delta() public {
         uint256 amount0 = Math.calcAmount0Delta(
-            TickMath.getSqrtRatioAtTick(85176),
-            TickMath.getSqrtRatioAtTick(86129),
-            1517882343751509868544
+            TickMath.getSqrtRatioAtTick(85176), TickMath.getSqrtRatioAtTick(86129), 1517882343751509868544
         );
 
         assertEq(amount0, 0.998833192822975409 ether);
@@ -18,9 +16,7 @@ contract MathTest is Test {
 
     function testCalcAmount1Delta() public {
         uint256 amount1 = Math.calcAmount1Delta(
-            TickMath.getSqrtRatioAtTick(84222),
-            TickMath.getSqrtRatioAtTick(85176),
-            1517882343751509868544
+            TickMath.getSqrtRatioAtTick(84222), TickMath.getSqrtRatioAtTick(85176), 1517882343751509868544
         );
 
         assertEq(amount1, 4999.187247111820044641 ether);
